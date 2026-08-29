a4c9bd2f31f88e77442d8e18db971fa137887322
munthernashar <munther.nashar@nashcon.de>
2026-08-29 22:54:50 +0200

    fix: ErrorBoundary als benannter Import in main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
-import { BrowserRouter } from 'react-router-dom';
-import { AppRouter } from './app/routes/AppRouter';
-import ErrorBoundary from './components/ErrorBoundary';
-import './styles/global.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './app/routes/AppRouter'
+import { ErrorBoundary } from './components/ErrorBoundary'
+import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary>
         <AppRouter />
-      </BrowserRouter>
-      <Analytics />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StrictMode>,
+)
