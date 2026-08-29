1882ee0b09a9f7431f2cd4951fa9a7480ad06a9a
munthernashar <munther.nashar@nashcon.de>
2026-08-29 22:59:30 +0200

    fix: AuthProvider oberhalb von ErrorBoundary in main.tsx platzieren

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './features/auth/AuthContext'
 import { AppRouter } from './app/routes/AppRouter'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <AppRouter />
-      </ErrorBoundary>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <AppRouter />
+        </ErrorBoundary>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 )
